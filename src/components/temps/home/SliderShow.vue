0fb11905e7ff29fbfeb2b6bcaa7cea5435403e15
Quan.Nguyen <quan.nguyen@vmodev.com>
2019-08-30 11:32:40 +0700

    init home page

--- src/components/temps/home/SliderShow.vue
+++ src/components/temps/home/SliderShow.vue
@@ -3,7 +3,7 @@
         <v-layout wrap pa-0>
             <v-flex xs12>
                 <div class="text-1">We make ...</div>
-                <div class="text-1">Talk ...</div>
+                <div class="text-2">Talk ...</div>
                 <div class="slideshow">3 img</div>
             </v-flex>
         </v-layout>
