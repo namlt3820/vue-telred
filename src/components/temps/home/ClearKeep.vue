c0ef61e7014f18c80c80cf40d5745981addb982e
namlt3820 <namlt3820@gmail.com>
2019-09-05 10:14:11 +0700

    style: change css to scss

--- src/components/temps/home/ClearKeep.vue
+++ src/components/temps/home/ClearKeep.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-container fluid class="content-grid">
-        <div class="fix-width">
-            <v-layout wrap pa-0>
-                <v-flex xs12 sm4 pa-0>
-                    <v-img v-if="$vuetify.breakpoint.xsOnly" src="@/assets/images/clear-keep-mobile.png"></v-img>
-                </v-flex>
-                <v-flex xs12 sm8 pa-4>
-                    <div class="text-0">
+    <div class="section-3-inner">
+        <v-container fluid class="content-grid">
+            <div class="fix-width">
+                <v-layout wrap pa-0>
+                    <v-flex xs12 sm4 xl5 pa-0>
+                        <v-img v-if="$vuetify.breakpoint.xsOnly" src="@/assets/images/clear-keep-mobile.png"></v-img>
+                    </v-flex>
+                    <v-flex xs12 sm8 xl7>
                         <div class="logo">
                             <img src="@/assets/images/clear-keep-icon.png" alt />
                             <div class="text-1">
@@ -19,7 +19,6 @@
                             <span style="color: #f9566d">only secure unified</span>
                             <br />communication and enterprise storage tool in the world
                         </div>
-                        <div class="text-3" style="color: #01348d"></div>
                         <v-btn
                             depressed
                             rounded
@@ -29,101 +28,105 @@
                             href="http://www.clearkeep.io"
                             >Read more</v-btn
                         >
-                    </div>
-                </v-flex>
-            </v-layout>
-        </div>
-    </v-container>
+                    </v-flex>
+                </v-layout>
+            </div>
+        </v-container>
+    </div>
 </template>
 <script>
 export default {
     name: 'LyncSkype'
 };
 </script>
-<style>
-.section-3 {
+<style lang="scss" scoped>
+@import 'src/assets/css/_size_classes.scss';
+.section-3-inner {
     height: 820px;
     display: flex;
     align-items: top;
-}
 
-@media only screen and (min-width: 600px) {
-    .section-3 {
-        height: 950px;
-        background: url(../../../assets/images/clear-keep.png) no-repeat bottom right;
-        background-size: 1640px auto;
+    @media only screen and (min-width: $small_screen) {
+        background: url(../../../assets/images/clear-keep.png);
+        background-size: 110%;
+        background-repeat: no-repeat;
         background-position-x: -200px;
+        width: 100%;
+        height: 0;
+        padding-bottom: 65%;
+        position: relative;
+        display: flex;
+        align-items: center;
     }
-}
 
-@media only screen and (min-width: 1920px) {
-    .section-3 {
-        height: 1100px;
-        background-position-x: 0px;
+    @media only screen and (min-width: $extra_large_screen) {
+        background-size: contain;
+        background-position-x: 0;
+        padding-bottom: 58%;
     }
-}
 
-.section-3 .content-grid .btn-read-more.theme--light.v-btn {
-    color: #fff;
-    text-transform: none;
-    font-size: 16px;
-    width: 160px;
-    height: 50px;
-    margin-top: 20px;
-}
+    .content-grid {
+        @media only screen and (min-width: $small_screen) {
+            position: absolute;
+            top: 350px;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+        @media only screen and (min-width: $large_screen) {
+            top: 100px;
+        }
+        @media only screen and (min-width: $extra_large_screen) {
+            top: 200px;
+        }
 
-@media only screen and (min-width: 600px) {
-    .section-3 .content-grid .btn-read-more.theme--light.v-btn {
-        width: 176px;
-        height: 60px;
-    }
-}
+        .btn-read-more.theme--light.v-btn {
+            color: #fff;
+            text-transform: none;
+            font-size: 16px;
+            width: 160px;
+            height: 50px;
+            margin-top: 20px;
 
-.section-3 .content-grid .logo {
-    display: flex;
-    color: #f9556d;
-    margin-bottom: 20px;
-}
+            @media only screen and (min-width: $small_screen) {
+                width: 176px;
+                height: 60px;
+            }
+        }
 
-.section-3 .content-grid .logo .t1 {
-    font-weight: bold;
-    line-height: 20px;
-    padding-top: 3px;
-}
-.section-3 .content-grid .logo .t2 {
-    font-size: 14px;
-    line-height: 14px;
-}
-.section-3 .content-grid .text-2 {
-    color: #01348d;
-    font-size: 24px;
-    font-weight: bold;
-    line-height: 40px;
-}
+        .logo {
+            display: flex;
+            color: #f9556d;
+            margin-bottom: 20px;
 
-@media only screen and (min-width: 600px) {
-    .section-3 .content-grid .text-2 {
-        font-size: 40px;
-        margin-bottom: 40px;
-        line-height: 50px;
-    }
-}
+            .t1 {
+                font-weight: bold;
+                line-height: 20px;
+                padding-top: 3px;
+            }
 
-.content-grid .fix-width {
-    position: relative;
-}
+            .t2 {
+                font-size: 14px;
+                line-height: 14px;
+            }
+        }
 
-@media only screen and (min-width: 600px) {
-    .text-0 {
-        padding-top: 100px;
-        padding-left: 50px;
-    }
-}
+        .text-2 {
+            color: #01348d;
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 40px;
 
-@media only screen and (min-width: 1920px) {
-    .text-0 {
-        padding-top: 100px;
-        padding-left: 0;
+            @media only screen and (min-width: $small_screen) {
+                font-size: 40px;
+                margin-bottom: 40px;
+                line-height: 50px;
+            }
+        }
+
+        .fix-width {
+            position: relative;
+        }
     }
 }
 </style>
--- src/components/temps/home/LyncSkype.vue
+++ src/components/temps/home/LyncSkype.vue
@@ -1,119 +1,139 @@
 <template>
-    <v-container fluid class="content-grid">
-        <v-layout wrap pa-0>
-            <v-flex xs12 v-if="$vuetify.breakpoint.xsOnly" px-3 mb-6>
-                <v-img src="@/assets/images/lync-skype-mobile.jpg" contain></v-img>
-            </v-flex>
-            <v-flex xs12 pa-md-0 pt-md-10 pa-4>
-                <div class="fix-width">
-                    <div class="logo">
-                        <img src="@/assets/images/lync-skype-icon.png" alt />
-                        <div class="text-1">
-                            <div class="t1">Lync/Skype for business</div>
-                            <div class="t2">www.tel.red</div>
+    <div class="section-2-inner">
+        <v-container fluid class="content-grid">
+            <v-layout wrap pa-0>
+                <v-flex xs12 v-if="$vuetify.breakpoint.xsOnly" px-3 mb-6>
+                    <v-img src="@/assets/images/lync-skype-mobile.jpg" contain></v-img>
+                </v-flex>
+                <v-flex xs12 pa-md-0 pt-md-10 pa-4>
+                    <div class="fix-width">
+                        <div class="logo">
+                            <img src="@/assets/images/lync-skype-icon.png" alt />
+                            <div class="text-1">
+                                <div class="t1">Lync/Skype for business</div>
+                                <div class="t2">www.tel.red</div>
+                            </div>
+                        </div>
+                        <div class="text-2">
+                            There was no Skype client
+                            <br />for Linux. So we <span style="color: #f9566d">built it</span>.
+                        </div>
+                        <v-btn
+                            depressed
+                            rounded
+                            class="btn-read-more"
+                            color="#F9556D"
+                            target="blank"
+                            href="http://www.tel.red"
+                            >Read more</v-btn
+                        >
+                        <div class="text-3" v-if="!$vuetify.breakpoint.xsOnly">
+                            Lync/Skype
+                            <br />for business
                         </div>
                     </div>
-                    <div class="text-2">
-                        There was no Skype client
-                        <br />for Linux. So we <span style="color: #f9566d">built it</span>.
-                    </div>
-                    <v-btn
-                        depressed
-                        rounded
-                        class="btn-read-more"
-                        color="#F9556D"
-                        target="blank"
-                        href="http://www.tel.red"
-                        >Read more</v-btn
-                    >
-                    <div class="text-3" v-if="!$vuetify.breakpoint.xsOnly">
-                        Lync/Skype
-                        <br />for business
-                    </div>
-                </div>
-            </v-flex>
-        </v-layout>
-    </v-container>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 <script>
 export default {
     name: 'LyncSkype'
 };
 </script>
-<style>
-.section-2 {
+<style scoped lang="scss">
+@import 'src/assets/css/_size_classes.scss';
+
+.section-2-inner {
     height: 550px;
-}
 
-@media only screen and (min-width: 600px) {
-    .section-2 {
-        background: url(../../../assets/images/lync-skype.png) no-repeat bottom right;
-        background-size: 1440px auto;
-        height: 800px;
+    .btn-read-more.theme--light.v-btn {
+        color: #fff;
+        text-transform: none;
+        font-size: 16px;
+        width: 160px;
+        height: 50px;
+        margin-top: 20px;
+
+        @media only screen and (min-width: $small_screen) {
+            width: 176px;
+            height: 60px;
+        }
     }
-}
 
-.section-2 .content-grid .btn-read-more.theme--light.v-btn {
-    color: #fff;
-    text-transform: none;
-    font-size: 16px;
-    width: 160px;
-    height: 50px;
-    margin-top: 20px;
-}
+    .logo {
+        display: flex;
+        color: #f9556d;
+        margin-bottom: 20px;
 
-@media only screen and (min-width: 600px) {
-    .section-2 .content-grid .btn-read-more.theme--light.v-btn {
-        width: 176px;
-        height: 60px;
+        img {
+            margin-right: 1rem;
+        }
+
+        .t1 {
+            font-weight: bold;
+            line-height: 20px;
+            padding-top: 3px;
+        }
+
+        .t2 {
+            font-size: 14px;
+            line-height: 14px;
+        }
     }
-}
 
-.section-2 .content-grid .logo {
-    display: flex;
-    color: #f9556d;
-    margin-bottom: 20px;
-}
-.section-2 {
-    display: flex;
-    align-items: center;
-}
-.section-2 .content-grid .logo img {
-    margin-right: 1rem;
-}
-.section-2 .content-grid .logo .t1 {
-    font-weight: bold;
-    line-height: 20px;
-    padding-top: 3px;
-}
-.section-2 .content-grid .logo .t2 {
-    font-size: 14px;
-    line-height: 14px;
-}
-.section-2 .content-grid .text-2 {
-    color: #01348d;
-    font-size: 24px;
-    font-weight: bold;
-    line-height: 40px;
-}
+    .text-2 {
+        color: #01348d;
+        font-size: 24px;
+        font-weight: bold;
+        line-height: 40px;
 
-@media only screen and (min-width: 600px) {
-    .section-2 .content-grid .text-2 {
-        font-size: 40px;
-        margin-bottom: 40px;
-        line-height: 50px;
+        @media only screen and (min-width: 600px) {
+            font-size: 40px;
+            margin-bottom: 40px;
+            line-height: 50px;
+        }
     }
-}
 
-.section-2 .content-grid .text-3 {
-    font-size: 90px;
-    color: #fff3f4;
-    font-weight: bold;
-    line-height: 100px;
-    margin-top: 30px;
-}
+    .text-3 {
+        font-size: 90px;
+        color: #fff3f4;
+        font-weight: bold;
+        line-height: 100px;
+        margin-top: 30px;
+    }
 
-.content-grid .fix-width {
-    position: relative;
+    .fix-width {
+        position: relative;
+    }
+
+    @media only screen and (min-width: 600px) {
+        background: url(../../../assets/images/lync-skype.png);
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 100%;
+        height: 0;
+        padding-bottom: 55%;
+        position: relative;
+        display: flex;
+        align-items: center;
+
+        .content-grid {
+            @media only screen and (min-width: $small_screen) {
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                top: 240px;
+            }
+            @media only screen and (min-width: $large_screen) {
+                top: 240px;
+            }
+            @media only screen and (min-width: $extra_large_screen) {
+                top: 350px;
+            }
+        }
+    }
 }
 </style>
