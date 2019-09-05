845e4afaad16dbc563d61701f69d30fbd053058d
Quan.Nguyen <quan.nguyen@vmodev.com>
2019-09-05 14:52:20 +0700

    Upd responsive section 4

--- src/components/temps/home/Twain.vue
+++ src/components/temps/home/Twain.vue
@@ -31,54 +31,91 @@ export default {
     name: 'LyncSkype'
 };
 </script>
-<style>
+<style lang="scss">
 .section-4 {
-    background: url(../../../assets/images/bg-twain.png) no-repeat bottom right;
+    background: url(../../../assets/images/bg-twain.png) no-repeat right bottom;
     background-size: auto 100%;
     height: 700px;
-}
-</style>
-<style lang="scss" scoped>
-.content-grid {
-    .btn-read-more.theme--light.v-btn {
-        color: #fff;
-        text-transform: none;
-        font-size: 16px;
-        width: 176px;
-        height: 60px;
-        margin-top: 20px;
-    }
-    .logo {
-        display: flex;
-        color: #f9556d;
-        margin-bottom: 20px;
-        img {
-            padding-right: 13px;
+
+    .content-grid {
+        .btn-read-more.theme--light.v-btn {
+            color: #fff;
+            text-transform: none;
+            font-size: 16px;
+            width: 176px;
+            height: 60px;
+            margin-top: 20px;
         }
-        .t1 {
+        .logo {
+            display: flex;
+            color: #f9556d;
+            margin-bottom: 20px;
+            img {
+                padding-right: 13px;
+            }
+            .t1 {
+                font-weight: bold;
+                line-height: 20px;
+                padding-top: 3px;
+            }
+            .t2 {
+                font-size: 14px;
+                line-height: 14px;
+            }
+        }
+        .text-2 {
+            color: #01348d;
+            font-size: 40px;
             font-weight: bold;
-            line-height: 20px;
-            padding-top: 3px;
         }
-        .t2 {
-            font-size: 14px;
-            line-height: 14px;
+        .text-3 {
+            color: #f9556d;
+            font-size: 40px;
+            font-weight: bold;
+            line-height: 38px;
+            margin-bottom: 40px;
+        }
+        .fix-width {
+            position: relative;
         }
     }
-    .text-2 {
-        color: #01348d;
-        font-size: 40px;
-        font-weight: bold;
+}
+@media only screen and (max-width: 1280px) and (min-width: 1025px) {
+    .section-4 {
+        background: url(../../../assets/images/bg-twain.png) no-repeat right -140px bottom -60px;
     }
-    .text-3 {
-        color: #f9556d;
-        font-size: 40px;
-        font-weight: bold;
-        line-height: 38px;
-        margin-bottom: 40px;
+}
+@media only screen and (max-width: 1024px) {
+    .section-4 {
+        background: url(../../../assets/images/bg-twain-1024.png) no-repeat right bottom;
     }
-    .fix-width {
-        position: relative;
+}
+@media only screen and (max-width: 768px) {
+    .section-4 {
+        background: url(../../../assets/images/bg-twain-1024.png) no-repeat right bottom;
+        background-size: 90% auto;
+        height: auto;
+        padding-bottom: 220px;
+    }
+}
+@media only screen and (max-width: 640px) {
+    .section-4 {
+        background-size: 500px auto;
+        padding-bottom: 230px;
+
+        .content-grid {
+            .text-2 {
+                font-size: 26px;
+            }
+            .text-3 {
+                font-size: 26px;
+                margin-bottom: 0;
+            }
+            .btn-read-more.theme--light.v-btn {
+                height: 40px;
+                font-size: 14px;
+            }
+        }
     }
 }
 </style>
